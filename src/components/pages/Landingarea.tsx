fbab2f14c824f198d4e317cfb20788c0886d3221
Tio Noviar <thenoviar@gmail.com>
2024-05-15 15:12:46 +0700

    fixed warning error

--- src/components/pages/Landingarea.tsx
+++ src/components/pages/Landingarea.tsx
@@ -46,7 +46,7 @@ function Landingarea() {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   clipRule="evenodd"
                   d="M-6.48499e-05 -2.09815e-06L24.5903 9.14421L48 0.439014L24.1936 27.4308L-6.48499e-05 -2.09815e-06Z"
                   fill="#2ABDFB"
