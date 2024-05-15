53e3556d718e2bbab52f6a4506230299f705b977
Tio Noviar <thenoviar@gmail.com>
2024-05-15 15:11:53 +0700

    fixed warning error

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -24,7 +24,7 @@ function ButtonA({ text, color }: { text: string; color: string }) {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             clipRule="evenodd"
             d="M0 32.7699L8.76057 15.982L0.420617 0L26.2799 16.2528L0 32.7699Z"
             fill={color}
