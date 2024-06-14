115515c67603704e9dd619e99dd32018ac698b98
Dafazan <dafazanlbs@gmail.com>
2024-06-14 20:16:37 +0700

    new navs

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -131,157 +131,170 @@ function Navbar() {
           </div>
         </div>
       </div>
-      {isSidebar == true ? (
-        <>
-          <div className="fixed w-full h-full flex z-50">
-            <div className="md:w-3/12 w-0 bg-black opacity-35"></div>
-            <div className="md:w-9/12 w-full h-screen bg-[#000820] text-[#2ABDFB] flex-flex-col">
-              <div className="md:h-24 h-16 w-full flex justify-between trans md:p-10 py-8 px-3">
-                <div></div>
-                <div onClick={CloseSidebar} className="relative md:pe-16 pe-12">
-                  <div
-                    className={`md:w-12 w-10 h-[5px] bg-[#2ABDFB] rotate-45 duration-150 absolute ${
-                      xbutton == true ? "" : "translate-x-32 translate-y-32"
-                    }`}
-                  ></div>
-                  <div
-                    className={`md:w-12 w-10 h-[5px] bg-[#2ABDFB] -rotate-45 duration-150 absolute ${
-                      xbutton == true ? "" : "translate-x-32 -translate-y-32"
-                    }`}
-                  ></div>
-                </div>
-              </div>
-              <div className="md:px-14 px-7 text-5xl  font-extrabold flex flex-col md:gap-5 gap-2">
-                <div className="flex">
-                  <div
-                    onMouseEnter={() => setLinkhover(1)}
-                    onMouseLeave={() => setLinkhover(0)}
-                    className="flex gap-3 "
-                  >
-                    {linkhover == 1 ? (
-                      <>
-                        <img src="/arrowonly.svg" alt="" />
-                      </>
-                    ) : (
-                      <></>
-                    )}
-                    <a
-                      className=" md:text-5xl text-2xl hover:text-[#00FFFF]"
-                      href="/#"
-                    >
-                      IT SOLUTION
-                    </a>
-                  </div>
-                </div>
-                <div className="flex">
-                  <div
-                    onMouseEnter={() => setLinkhover(2)}
-                    onMouseLeave={() => setLinkhover(0)}
-                    className="flex gap-3 "
-                  >
-                    {linkhover == 2 ? (
-                      <>
-                        <img src="/arrowonly.svg" alt="" />
-                      </>
-                    ) : (
-                      <></>
-                    )}
-                    <a
-                      className=" md:text-5xl text-2xl hover:text-[#00FFFF]"
-                      href="/#"
-                    >
-                      FARMTECH
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className=" h-[2px] md:mx-14 m-7 bg-[#2ABDFB]"></div>
-              <div className="md:px-14 px-7 md:text-3xl text-xl font-extrabold flex outline-text flex-col md:gap-3 gap-2">
-                <div className="flex">
-                  <div
-                    onMouseEnter={() => setLinkhover(3)}
-                    onMouseLeave={() => setLinkhover(0)}
-                    className="flex gap-2 "
-                  >
-                    {linkhover == 3 ? (
-                      <>
-                        <img className="scale-75" src="/arrowonly.svg" alt="" />
-                      </>
-                    ) : (
-                      <></>
-                    )}
-                    <a className="hover:text-[#00FFFF] " href="/portfolio">
-                      PORTFOLIO
-                    </a>
-                  </div>
-                </div>
-
-                <div className="flex">
-                  <div
-                    onMouseEnter={() => setLinkhover(4)}
-                    onMouseLeave={() => setLinkhover(0)}
-                    className="flex gap-2 "
-                  >
-                    {linkhover == 4 ? (
-                      <>
-                        <img className="scale-75" src="/arrowonly.svg" alt="" />
-                      </>
-                    ) : (
-                      <></>
-                    )}
-                    <a className="hover:text-[#00FFFF] " href="/about">
-                      ABOUT US
-                    </a>
-                  </div>
-                </div>
-
-                <div className="flex">
-                  <div
-                    onMouseEnter={() => setLinkhover(5)}
-                    onMouseLeave={() => setLinkhover(0)}
-                    className="flex gap-2 "
-                  >
-                    {linkhover == 5 ? (
-                      <>
-                        <img className="scale-75" src="/arrowonly.svg" alt="" />
-                      </>
-                    ) : (
-                      <></>
-                    )}
-                    <a className="hover:text-[#00FFFF] " href="/contact">
-                      CONTACT US
-                    </a>
-                  </div>
-                </div>
-
-                <div className="flex">
-                  <div
-                    onMouseEnter={() => setLinkhover(6)}
-                    onMouseLeave={() => setLinkhover(0)}
-                    className="flex gap-2 "
-                  >
-                    {linkhover == 6 ? (
-                      <>
-                        <img className="scale-75" src="/arrowonly.svg" alt="" />
-                      </>
-                    ) : (
-                      <></>
-                    )}
-                    <a className="hover:text-[#00FFFF] " href="/information">
-                      INFORMATION AND NEWS
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className=" h-[2px] md:mx-14 m-7 bg-[#2ABDFB]"></div>
-              <SocialLink />
+      {/* {isSidebar == true ? (
+        <> */}
+      <div
+        className={`${
+          isSidebar == true
+            ? ""
+            : "md:translate-x-[100%] md:translate-y-0 -translate-y-[100%]"
+        } fixed w-full h-full flex z-50 duration-300`}
+      >
+        <div
+          className={`md:w-3/12 w-0 bg-black opacity-35 duration-[3000ms] ${
+            isSidebar == true ? "" : "opacity-0"
+          }`}
+        ></div>
+        <div className="md:w-9/12 w-full h-screen bg-[#000820] text-[#2ABDFB] flex-flex-col">
+          <div className="md:h-24 h-16 w-full flex justify-between trans md:p-10 py-8 px-3">
+            <div></div>
+            <div
+              onClick={CloseSidebar}
+              className="relative md:pe-16 pe-12 z-50"
+            >
+              <div
+                className={`md:w-12 w-10 h-[5px] bg-[#2ABDFB] rotate-45 duration-150 absolute ${
+                  xbutton == true ? "" : "translate-x-32 translate-y-32"
+                }`}
+              ></div>
+              <div
+                className={`md:w-12 w-10 h-[5px] bg-[#2ABDFB] -rotate-45 duration-150 absolute ${
+                  xbutton == true ? "" : "translate-x-32 -translate-y-32"
+                }`}
+              ></div>
             </div>
           </div>
-        </>
-      ) : (
-        <></>
-      )}
+          <div className="md:px-14 px-7 text-5xl  font-extrabold flex flex-col md:gap-5 gap-2">
+            <div className="flex">
+              <div
+                onMouseEnter={() => setLinkhover(1)}
+                onMouseLeave={() => setLinkhover(0)}
+                className="flex gap-3 "
+              >
+                {linkhover == 1 ? (
+                  <>
+                    <img src="/arrowonly.svg" alt="" />
+                  </>
+                ) : (
+                  <></>
+                )}
+                <a
+                  className=" md:text-5xl text-2xl hover:text-[#00FFFF]"
+                  href="/#"
+                >
+                  IT SOLUTION
+                </a>
+              </div>
+            </div>
+            <div className="flex">
+              <div
+                onMouseEnter={() => setLinkhover(2)}
+                onMouseLeave={() => setLinkhover(0)}
+                className="flex gap-3 "
+              >
+                {linkhover == 2 ? (
+                  <>
+                    <img src="/arrowonly.svg" alt="" />
+                  </>
+                ) : (
+                  <></>
+                )}
+                <a
+                  className=" md:text-5xl text-2xl hover:text-[#00FFFF]"
+                  href="/#"
+                >
+                  FARMTECH
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className=" h-[2px] md:mx-14 m-7 bg-[#2ABDFB]"></div>
+          <div className="md:px-14 px-7 md:text-3xl text-xl font-extrabold flex outline-text flex-col md:gap-3 gap-2">
+            <div className="flex">
+              <div
+                onMouseEnter={() => setLinkhover(3)}
+                onMouseLeave={() => setLinkhover(0)}
+                className="flex gap-2 "
+              >
+                {linkhover == 3 ? (
+                  <>
+                    <img className="scale-75" src="/arrowonly.svg" alt="" />
+                  </>
+                ) : (
+                  <></>
+                )}
+                <a className="hover:text-[#00FFFF] " href="/portfolio">
+                  PORTFOLIO
+                </a>
+              </div>
+            </div>
+
+            <div className="flex">
+              <div
+                onMouseEnter={() => setLinkhover(4)}
+                onMouseLeave={() => setLinkhover(0)}
+                className="flex gap-2 "
+              >
+                {linkhover == 4 ? (
+                  <>
+                    <img className="scale-75" src="/arrowonly.svg" alt="" />
+                  </>
+                ) : (
+                  <></>
+                )}
+                <a className="hover:text-[#00FFFF] " href="/about">
+                  ABOUT US
+                </a>
+              </div>
+            </div>
+
+            <div className="flex">
+              <div
+                onMouseEnter={() => setLinkhover(5)}
+                onMouseLeave={() => setLinkhover(0)}
+                className="flex gap-2 "
+              >
+                {linkhover == 5 ? (
+                  <>
+                    <img className="scale-75" src="/arrowonly.svg" alt="" />
+                  </>
+                ) : (
+                  <></>
+                )}
+                <a className="hover:text-[#00FFFF] " href="/contact">
+                  CONTACT US
+                </a>
+              </div>
+            </div>
+
+            <div className="flex">
+              <div
+                onMouseEnter={() => setLinkhover(6)}
+                onMouseLeave={() => setLinkhover(0)}
+                className="flex gap-2 "
+              >
+                {linkhover == 6 ? (
+                  <>
+                    <img className="scale-75" src="/arrowonly.svg" alt="" />
+                  </>
+                ) : (
+                  <></>
+                )}
+                <a className="hover:text-[#00FFFF] " href="/information">
+                  INFORMATION AND NEWS
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className=" h-[2px] md:mx-14 m-7 bg-[#2ABDFB]"></div>
+          <SocialLink />
+        </div>
+      </div>
     </>
+    // ) : (
+    //   <></>
+    // )}
+    // </>
   );
 }
 
