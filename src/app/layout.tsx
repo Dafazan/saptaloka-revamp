b5fab20a43bb6d1374d22c5e6ff26d6995c1e767
Dafazan <dafazanlbs@gmail.com>
2024-06-22 15:17:23 +0700

    whatsapp button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import WhatsappShortcut from "@/components/quickwhatsapp";
 
 const inter = Montserrat({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
             <Footer />
           </div>
           <Navbar />
+          <WhatsappShortcut />
         </div>
       </body>
     </html>
