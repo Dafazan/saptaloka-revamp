ac553d6cedc5518a8dd5747a6e55896f93bcf318
Dafazan <dafazanlbs@gmail.com>
2024-07-02 16:26:27 +0700

    tgs

---
+++ src/app/prakerin/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Prakerin() {
+  return (
+    <div className="w-screen h-screen p-[4%] pt-32">
+      <div className="grid grid-cols-4">
+        <a
+          href="/prakerin/uiux"
+          className="bg-white rounded-md flex flex-col p-5"
+        >
+          <h2 className="font-semibold text-xl">UI/UX</h2>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Prakerin;
