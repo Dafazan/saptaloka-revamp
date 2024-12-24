07998ba22976dc795b90a2a5e666f36f26940274
Daffa Fauzan <dafazanlbs@gmail.com>
2024-12-24 14:30:18 +0700

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -14,7 +14,7 @@ function Newpage() {
       <div className="md:w-3/6 w-full p-[4%] flex md:justify-end justify-center">
         <Image
           src={
-            "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdmo4OXprZDZqczRxemRmY3duM2trZmQ0dmNvdjhoZ3F5YzU0aGU0ZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UK7Io2fmGTJ4ly6Kf1/giphy.gif"
+            "#"
           }
           height={500}
           width={500}
